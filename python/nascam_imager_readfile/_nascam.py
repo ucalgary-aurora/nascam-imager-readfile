7b43de1bd3fb6f61a98dd1a7a5bf198fb3217ec4
Darren Chaddock <dchaddoc@ucalgary.ca>
2024-02-03 11:26:45 -0700

    bugfix for working dir creation

--- python/nascam_imager_readfile/_nascam.py
+++ python/nascam_imager_readfile/_nascam.py
@@ -170,7 +170,7 @@ def read(file_list, workers=1, tar_tempdir=None, quiet=False):
     if (tar_tempdir is None):
         tar_tempdir = Path("%s/.nascam_imager_readfile" % (str(Path.home())))
     if not (os.path.exists(tar_tempdir)):
-        os.makedirs(tar_tempdir)
+        os.makedirs(tar_tempdir, exist_ok=True)
 
     # if input is just a single file name in a string, convert to a list to be fed to the workers
     if isinstance(file_list, str):
